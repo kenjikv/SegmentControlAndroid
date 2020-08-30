d79acacec7a99765f63c2023555d0ee594c4027d
Kenji Kawaida <kenjikawaidav@gmail.com>
2020-08-30 18:07:21 -0400

    init commit

---
+++ app/src/main/java/com/kawaida/segmentcontrolandroid/MainActivity.java
@@ -0,0 +1,32 @@
+package com.kawaida.segmentcontrolandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ((RadioGroup)findViewById(R.id.radioGroup)).setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                RadioButton rd = radioGroup.findViewById(i);
+                ((TextView)findViewById(R.id.text)).setText(rd.getText());
+            }
+        });
+
+    }
+}
